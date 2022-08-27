9972fe11073f610096ce19d63db7e8e3766412d4
Vasika-uso <iovunicu@mail.ru>
2022-08-27 15:28:34 +0300

    fixed buggy drive letter generated by sh script

--- src/native/wgen-managed.bat
+++ src/native/wgen-managed.bat
@@ -10,7 +10,19 @@ cd ./native
 mkdir generated
 cd ..
 cd ..
-sh "./src/native/gen-managed.sh" "./out/CsharpSample/bin/Debug/net6.0/win-x86/publish"  1>  "./out/native/generated/tpa.h"  
+sh "./src/native/gen-managed.sh" "./out/CsharpSample/bin/Debug/net6.0/win-x86/publish" 1> "./out/native/generated/tpa.h"  
+
+:: This replaces /c/ and other buggy drive letters ( just add a letter :/ )
+powershell -Command "(gc ./out/native/generated/tpa.h) -replace '/c/', 'C:/' | Out-File -encoding ASCII ./out/native/generated/tpa.h"
+powershell -Command "(gc ./out/native/generated/tpa.h) -replace '/d/', 'D:/' | Out-File -encoding ASCII ./out/native/generated/tpa.h"
+powershell -Command "(gc ./out/native/generated/tpa.h) -replace '/e/', 'E:/' | Out-File -encoding ASCII ./out/native/generated/tpa.h"
+powershell -Command "(gc ./out/native/generated/tpa.h) -replace '/f/', 'F:/' | Out-File -encoding ASCII ./out/native/generated/tpa.h"
+powershell -Command "(gc ./out/native/generated/tpa.h) -replace '/g/', 'G:/' | Out-File -encoding ASCII ./out/native/generated/tpa.h"
+powershell -Command "(gc ./out/native/generated/tpa.h) -replace '/h/', 'H:/' | Out-File -encoding ASCII ./out/native/generated/tpa.h"
+ 
+ 
+ 
+ 
  
 
          
